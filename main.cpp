c20811959c490b9ac6940a5703c6f6c43ee6463c
mohamedabdelmohymen <thelastofus1res@gmail.com>
2025-11-12 15:15:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,37 @@
 #include "Visitor.h"
 #include <iostream>
 using namespace std;
-
+Visitor:: Visitor(string& visitorName,int ticketsBiought){
+  string  this.visitorName->visitorName;
+  int this.ticketsBiought->ticketsBiought;
+  }
+  Visitor:: Visitor(){
+  string   visitorName = ' ';
+  int  ticketsBiought=0;
+  }
+  void Visitor::setticketsBiought(){
+ int ticketsBiought->ticketsBiought;
+}
+int Visitor::getticketsBiought(){
+return ticketsBiought;
+}
+  void Visitor::setvisitorName(){
+ string visitorName->visitorName;
+}
+string Visitor::getvisitorName(){
+return ticketsBiought;
+}
+ void Visitor::displayBoughtInfo(){
+     cout<<'visitorName'<<visitorName<<'ticketsBiought'<<ticketsBiought<<endl;
+ }
+Enclosure::Enclosure(){
+int this.capacity=0;
+int this.currentCount=0;
+}
+Enclosure::Enclosure(Animal*a,int capacity,int currentCount){
+int this.capacity->capacity;
+int this.currentCount->currentCount;
+}
 
 // ============== MAIN FUNCTION ==============
 int main() {
